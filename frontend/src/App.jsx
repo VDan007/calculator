537d5975f785cabcfd88a041f59ef406da579b81
DanielVarjaskéri <varjaskerid@gmail.com>
2023-04-20 01:21:25 +0200

    logic taking shape

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,29 @@ import { useState } from 'react';
 
 function App() {
 
-  const [current,setCurrent] = useState('');
-  const [previous,setPrevious] = useState('');
+  const [currentOperand,setCurrentOperand] = useState('');
+  const [previousOperand,setPreviousOperand] = useState('');
   const [operation,setOperation] = useState('');
 
   
-  function addNumberToCurrent(e){
-    const newNum = String(current) + e.target.innerText;
-    setCurrent(newNum);
+  function addNumberToCurrentOperand(e){
+    if(e.target.innerText !== '.'){
+      const newNum = String(currentOperand) + e.target.innerText;
+      setCurrentOperand(newNum);
+    } else if (e.target.innerText === '.' && !currentOperand.includes('.')){
+      const newNum = String(currentOperand) + e.target.innerText;
+      setCurrentOperand(newNum);
+    }
+  }
+
+  function addNumberToPreviousOperand(){
+    setPreviousOperand(currentOperand);
+    setCurrentOperand('');
+  }
+
+  function handleClickOperation(e){
+    setOperation(e.target.innerText);
+    addNumberToPreviousOperand();
   }
 
   return (
@@ -19,31 +34,31 @@ function App() {
 
     <div className='output'>
       <div className='prev'>
-        <p>{previous}{operation}</p>
+        <p>{previousOperand}{operation}</p>
       </div>
       <div className='current'>
-        <p>{current}</p>
+        <p>{currentOperand}</p>
       </div>
     </div>
 
     <button className='normal pinkLetter'>C</button>
     <button className='normal pinkLetter'>M+</button>
     <button className='normal pinkLetter'>Ma</button>
-    <button className='pinkBg'>÷</button>
-    <button className='normal' onClick={addNumberToCurrent}>7</button>
-    <button className='normal' onClick={addNumberToCurrent}>8</button>
-    <button className='normal' onClick={addNumberToCurrent}>9</button>
-    <button className='pinkBg'>x</button>
-    <button className='normal' onClick={addNumberToCurrent}>4</button>
-    <button className='normal' onClick={addNumberToCurrent}>5</button>
-    <button className='normal' onClick={addNumberToCurrent}>6</button>
-    <button className='pinkBg'>-</button>
-    <button className='normal' onClick={addNumberToCurrent}>1</button>
-    <button className='normal' onClick={addNumberToCurrent}>2</button>
-    <button className='normal' onClick={addNumberToCurrent}>3</button>
-    <button className='pinkBg'>+</button>
-    <button className='normal two__span'>0</button>
-    <button className='normal' onClick={addNumberToCurrent}>.</button>
+    <button className='pinkBg' onClick={handleClickOperation}>÷</button>
+    <button className='normal' onClick={addNumberToCurrentOperand}>7</button>
+    <button className='normal' onClick={addNumberToCurrentOperand}>8</button>
+    <button className='normal' onClick={addNumberToCurrentOperand}>9</button>
+    <button className='pinkBg' onClick={handleClickOperation}>x</button>
+    <button className='normal' onClick={addNumberToCurrentOperand}>4</button>
+    <button className='normal' onClick={addNumberToCurrentOperand}>5</button>
+    <button className='normal' onClick={addNumberToCurrentOperand}>6</button>
+    <button className='pinkBg' onClick={handleClickOperation}>-</button>
+    <button className='normal' onClick={addNumberToCurrentOperand}>1</button>
+    <button className='normal' onClick={addNumberToCurrentOperand}>2</button>
+    <button className='normal' onClick={addNumberToCurrentOperand}>3</button>
+    <button className='pinkBg' onClick={handleClickOperation}>+</button>
+    <button className='normal two__span' onClick={addNumberToCurrentOperand}>0</button>
+    <button className='normal' onClick={addNumberToCurrentOperand}>.</button>
     <button className='pinkBg'>=</button>
     
    </div>
